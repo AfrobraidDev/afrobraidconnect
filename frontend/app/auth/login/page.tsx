8f7ac4d9f0a1fb247f8a602fa3f8732247d98358
Nithullnolan@gmail.com <nwaukwanolan@gmail.com>
2025-08-18 13:35:08 +0100

    updated the api endpoints 18th August 2025

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -31,7 +31,7 @@ export default function LoginPage() {
 
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_AUTH_URL}/login/`,
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login/`,
         credentials,
         {
           headers: {
